01b17f5141c5123875742ca96a5335d85811a789
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-10-09 20:11:25 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<p> Дата и время:
+<p>
+<body style = "background: url(https://yt3.ggpht.com/a/AATXAJxgFA7Gy1y4szVT9zNWCIMWO4m0QvHuA1K9qCiNlA=s900-c-k-c0xffffffff-no-rj-mo); background-repeat: no-repeat;" >
+</body>
+<?php
+    $d=date("d.m.Y H:i");
+    echo($d);
+?>
+<ul id="nav">
+    <li><a href="2.4.php">Задание 2.4 В-1</a>
+    <li><a href="2.4.2.php">Задание 2.4 В-5</a>   
+    <li><a href="2.5.php">Задание 2.5 В-1</a>  
+    <li><a href="2.5.2.php">Задание 2.5 В-5</a>  
+    <li><a href="2.6.php">Задание 2.6 В-1</a>  
+    <li><a href="2.6.2.php">Задание 2.6 В-5</a>  
+</ul>
