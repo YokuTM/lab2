de8fd2379cc2d471dcfacfd4ec1b03e9c54e851f
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 03:43:41 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,18 +1,11 @@
-
-ЛР № 2 </br>
-<ul id="nav">
-    <li><a href="2.4.php">Задание 2.4 В-1</a>
-    <li><a href="2.4.2.php">Задание 2.4 В-5</a>   
-    <li><a href="2.5.php">Задание 2.5 В-1</a>  
-    <li><a href="2.5.2.php">Задание 2.5 В-5</a>  
-    <li><a href="2.6.php">Задание 2.6 В-1</a>  
-    <li><a href="2.6.2.php">Задание 2.6 В-5</a>
-</ul> 
-ЛР № 3 </br>
-<ul id="nav">
-    <li><a href="3.4.php">Задание 3.4 </a>  
-    <li><a href="3.5.php">Задание 3.5 </a>  
-    <li><a href="3.6.php">Задание 3.6 В-1</a>  
-    <li><a href="3.6.3.php">Задание 3.6 В-3</a> 
-    <li><a href="3.6.2.php">Задание 3.6 В-5</a> 
-</ul>
+<html>
+<head> <title> Сведения о прользователях сайта </title> </head>
+<body>
+<?php
+ mysql_connect("localhost", "root") or die ("Невозможно
+подключиться к серверу"); // установление соединения с сервером
+ mysql_query('SET NAMES cp1251'); // тип кодировки
+ // подключение к базе данных:
+ mysql_select_db("users") or die("Нет такой таблицы!");
+?>
+</body> </html>
