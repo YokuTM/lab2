b1abc38cb84f1044c3ed59029186f6360821398d
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 04:18:48 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,19 +1,4 @@
-<html>
-<head> <title> Сведения о прользователях сайта </title> </head>
-<body>
-<?php
-$link = mysqli_connect("127.0.0.1", "root", " ", "users");
 
-if (!$link) {
-    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Соединение с MySQL установлено!" . PHP_EOL;
-?>
-</body> </html>
 <ul id="nav">
     <li><a href="delete.php">Удаление </a>  
 </ul>
