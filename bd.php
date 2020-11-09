3d26f064e35688704b83fa3044dda61270400ec8
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 04:06:02 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -9,3 +9,26 @@
  mysqli_select_db("users") or die("Нет такой таблицы!");
 ?>
 </body> </html>
+<h2>Зарегистрированные пользователи:</h2>
+<table border="1">
+<tr> // вывод «шапки» таблицы
+ <th> Имя </th> <th> E-mail </th>
+ <th> Редактировать </th> <th> Уничтожить </th> </tr>
+<?php
+$result=mysql_query("SELECT id_user, user_name, user_e_mail
+FROM user"); // запрос на выборку сведений о пользователях
+while ($row=mysql_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+ echo "<td>" . $row['user_name'] . "</td>";
+ echo "<td>" . $row['user_e_mail'] . "</td>";
+ echo "<td><a href='edit.php?id=" . $row['id']
+. "'>Редактировать</a></td>"; // запуск скрипта для редактирования
+ echo "<td><a href='delete.php?id=" . $row['id']
+. "'>Удалить</a></td>"; // запуск скрипта для удаления записи
+ echo "</tr>";
+}
+print "</table>";
+$num_rows = mysql_num_rows($result); // число записей в таблице БД
+print("<P>Всего пользователей: $num_rows </p>");
+?>
+<p> <a href="new.html"> Добавить пользователя </a>
