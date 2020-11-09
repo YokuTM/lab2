a3c5ccfe2375b5d197a09d0ebf9902d19faf9245
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 04:01:20 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,7 +2,7 @@
 <head> <title> Сведения о прользователях сайта </title> </head>
 <body>
 <?php
- mysqli_connect("localhost", "root") or die ("Невозможно
+ mysqli_connect("localhost/Tools/phpMyAdmin/index.php?db=users&token=da2353399f73c23341e733f0c3299e4c", "root") or die ("Невозможно
 подключиться к серверу"); // установление соединения с сервером
  mysqli_query('SET NAMES cp1251'); // тип кодировки
  // подключение к базе данных:
