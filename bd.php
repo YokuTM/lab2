5280f4561a4b362fe7da833e8ced19e43b7c84c7
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 03:59:19 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,33 +2,10 @@
 <head> <title> Сведения о прользователях сайта </title> </head>
 <body>
 <?php
- mysql_connect("localhost", "root") or die ("Невозможно
+ mysqli_connect("localhost", "root") or die ("Невозможно
 подключиться к серверу"); // установление соединения с сервером
- mysql_query('SET NAMES cp1251'); // тип кодировки
+ mysqli_query('SET NAMES cp1251'); // тип кодировки
  // подключение к базе данных:
- mysql_select_db("users") or die("Нет такой таблицы!");
+ mysqli_select_db("users") or die("Нет такой таблицы!");
 ?>
 </body> </html>
-<h2>Зарегистрированные пользователи:</h2>
-<table border="1">
-<tr> // вывод «шапки» таблицы
- <th> Имя </th> <th> E-mail </th>
- <th> Редактировать </th> <th> Уничтожить </th> </tr>
-<?php
-$result=mysql_query("SELECT id_user, user_name, user_e_mail
-FROM user"); // запрос на выборку сведений о пользователях
-while ($row=mysql_fetch_array($result)){// для каждой строки из запроса
- echo "<tr>";
- echo "<td>" . $row['user_name'] . "</td>";
- echo "<td>" . $row['user_e_mail'] . "</td>";
- echo "<td><a href='edit.php?id=" . $row['id']
-. "'>Редактировать</a></td>"; // запуск скрипта для редактирования
- echo "<td><a href='delete.php?id=" . $row['id']
-. "'>Удалить</a></td>"; // запуск скрипта для удаления записи
- echo "</tr>";
-}
-print "</table>";
-$num_rows = mysql_num_rows($result); // число записей в таблице БД
-print("<P>Всего пользователей: $num_rows </p>");
-?>
-<p> <a href="new.html"> Добавить пользователя </a>
