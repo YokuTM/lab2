76f28af80c08456175a2cf5dcb9ca122ed51429f
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 04:43:17 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ define('DB_USER','root');
 define('DB_PASSWORD','');
 define('DB_NAME','users');
 	$mysql = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-if ($mysql->connect_errno) exit('Ощибка');
+if ($mysql->connect_errno) exit('Ошибка');
 	$mysql->set_charset('utf8');
 	$mysql->close();
 ?>
