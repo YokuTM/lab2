a5b82260786238f8391bb9dfe08df6c0c1c30fe0
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 04:09:16 +0500

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,10 +2,15 @@
 <head> <title> Сведения о прользователях сайта </title> </head>
 <body>
 <?php
- mysqli_connect("localhost", "root") or die ("Невозможно
-подключиться к серверу"); // установление соединения с сервером
- mysqli_query('SET NAMES cp1251'); // тип кодировки
- // подключение к базе данных:
- mysqli_select_db("users") or die("Нет такой таблицы!");
+$link = mysqli_connect("localhost", "root", "1234567", "users");
+
+if (!$link) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Соединение с MySQL установлено!" . PHP_EOL;
 ?>
 </body> </html>
