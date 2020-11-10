0a26d0f1a805f8efa48b16a678cb52d33b01e332
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 13:24:07 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,13 @@
 <?php
-$mysql = mysqli_connect('localhost','root', '', 'users');
-	
-if (!$mysql){ 
-die ('Ошибка подключения');
+$servername= "localhost";
+$username="root";
+$password="";
+try{
+	$mysql = new POD("mysql:host=$servername,dbname=users",$username,$password);
+	echo "Вход";
+
+}
+	cath (PDOException $e){
+		echo "НЕТ".$e->getMessage();
 }
 ?>
