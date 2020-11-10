d998d44c248733195e47c54ac4ea74d6a4374d5a
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 12:50:12 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
-
-$mysql=mysqli_connect("localhost", "root") or die ("Невозможно
-подключиться к серверу");
-mysql_query('SET NAMES cp1251');
+$mysql = mysqli_connect('localhost','root', '', 'users');
+	
+if (!$mysql){ 
+die ('Ошибка подключения');
+}
 ?>
