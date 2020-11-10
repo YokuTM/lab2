626db9835266bec0aefdfbe68bdfdd8ad8b7b61c
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-10 13:33:32 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,6 +2,9 @@
 $mysql = mysqli_connect('localhost','root', '', 'users');
 	
 if (!$mysql){ 
-die ('Ошибка подключения');
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 ?>
