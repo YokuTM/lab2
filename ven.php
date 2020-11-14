31b526a8da3b4eda79c5aa917685ff08f9252a4c
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-14 22:45:56 +0500

    Update ven.php

--- ven.php
+++ ven.php
@@ -9,7 +9,6 @@
 		mysqli_query($mysql, "INSERT INTO `student` (`full_name`, `faculty`, `groupa`, `num_report_card`, `number`) VALUES ('$fio', '$faq', '$group', '$zach', '$phone')");
         mysqli_query($mysql, "set @autoid :=0");
        mysqli_query($mysql, " update student set id_user = @autoid := (@autoid+1)");
-        //mysqli_query($mysql, "ALTER TABLE student AUTO_INCREMENT =1");
 		 header('Location: bd.php');
 
 		?>
