00a4cc6edf2081a25f89de5cde0851e104a3a4a4
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-14 22:21:32 +0500

    Update ven.php

--- ven.php
+++ ven.php
@@ -1,11 +1,15 @@
 <?php
 	require_once 'connect.php';
-		$id=$_POST['id_user'];
+		//$id=$_POST['id_user'];
 		$fio=$_POST['full_name'];
 		$faq=$_POST['faculty'];
 		$group=$_POST['groupa'];
 		$zach=$_POST['num_report_card'];
 		$phone=$_POST['number'];
-		mysqli_query($mysql, "INSERT INTO `student` (`id_user`,`full_name`, `faculty`, `groupa`, `num_report_card`, `number`) VALUES ('$id','$fio', '$faq', '$group', '$zach', '$phone')");
-		 header ('Location: bd.php');
+		mysqli_query($mysql, "INSERT INTO `student` (`full_name`, `faculty`, `groupa`, `num_report_card`, `number`) VALUES ('$fio', '$faq', '$group', '$zach', '$phone')");
+        mysqli_query($mysql, "set @autoid :=0");
+       mysqli_query($mysql, " update student set id_user = @autoid := (@autoid+1)");
+        mysqli_query($mysql, "ALTER TABLE student AUTO_INCREMENT =1");
+		 header('Location: bd.php');
+
 		?>
