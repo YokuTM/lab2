9e4683e46a797b590e45a56db88e798a4999714d
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-14 22:22:10 +0500

    Update del.php

--- del.php
+++ del.php
@@ -2,8 +2,10 @@
 require_once'connect.php';
 $fio=$_GET['full_name'];
 mysqli_query($mysql, "DELETE FROM `student` WHERE `student`.`full_name`='$fio'");
-
-header ('Location: bd.php');
+mysqli_query($mysql, "set @autoid :=0");
+       mysqli_query($mysql, " update student set id_user = @autoid := (@autoid+1)");
+        mysqli_query($mysql, "ALTER TABLE student AUTO_INCREMENT =1");
+header('Location: bd.php');
 
 
 ?>
