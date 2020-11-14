fc01f53e544ce3348ee236e91d3f9d8a3def9a2e
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-14 22:50:41 +0500

    Update del.php

--- del.php
+++ del.php
@@ -1,7 +1,6 @@
 <?php
 require_once'connect.php';
-//$fio=$_GET['full_name'];
-$zach=$_GET['num_report_card'];
+$fio=$_GET['full_name'];
 mysqli_query($mysql, "DELETE FROM `student` WHERE `student`.`num_report_card`='$zach'");
 mysqli_query($mysql, "set @autoid :=0");
 mysqli_query($mysql, " update student set id_user = @autoid := (@autoid+1)");
