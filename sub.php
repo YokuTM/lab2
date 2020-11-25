8634c68a293cf5ebbb66d2e7f56bbfc4fe01c4e1
Vlad <58938182+YokuTM@users.noreply.github.com>
2020-11-25 23:50:48 +0500

    Update sub.php
    добавление кнопки возврата

--- sub.php
+++ sub.php
@@ -26,6 +26,7 @@
 			<th>ID</th>
 			<th>НАЗВАНИЕ</th>
 			<th>ФИО ПРЕПОДАВАТЕЛЯ</th>
+			<th><a href=" index2.php">Вернуться в меню</a></th>
 		</tr>
 		<?php
 			$stud=mysqli_query($mysql, "SELECT * FROM `sub`");
